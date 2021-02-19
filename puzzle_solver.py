f69f1646a40d50327359f5b0229127b4d656ae54
Jack Barrell <jbarrell@live.co.uk>
2021-02-19 19:39:54 +0000

    cleaned up output

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -8,38 +8,33 @@ def load_words():
     with open('last_names.all.txt') as word_file:
         last_names = set(word_file.read().split())
 
-
-
     return valid_words, first_names, last_names
+
+
 
 def puzzler(puzzle):
     first_word, second_word = puzzle.split(' ')
     possible_first_names = []
     possible_second_names = []
 
-    i = 0
+    # print('FIRST WORD:')
+
     for name in last_names:
-        i += 1
-        if i % 1000 == 0:
-            print(round(i/(len(last_names) +len(first_names)) * 100), '%')
 
         word = first_word.replace('*', name)
         # print(word)
         if word in english_words:
-            print('found a fkn word!!: ', word)
+            # print('found a fkn word!!: ', word)
             possible_first_names.append((name, word))
 
 
-    print('MOVING ON TO SECOND WORD')
+    # print('\nSECOND WORD:')
     for name in last_names:
-        i += 1
-        if i % 1000 == 0:
-            print(round(i/(len(last_names) +len(first_names)) * 100), '%')
 
         word = second_word.replace('*', name)
         # print(word)
         if word in english_words:
-            print('found a fkn word!!: ', word)
+            # print('found a fkn word!!: ', word)
             possible_second_names.append((name, word))
 
     possible_first_names.sort()
@@ -53,17 +48,18 @@ def puzzler(puzzle):
     # for name in possible_second_names:
     #     print(name)
 
+    print('\nPossible celebs:')
     for first in possible_first_names:
         for second in possible_second_names:
-            print(first[0], second[0])
+            print(first[0].title(), second[0].title())
 
 
 if __name__ == '__main__':
     english_words, first_names, last_names = load_words()
 
     puzzle = '*sled re*nt'
-    puzzle = '*stro sou*er'
-    puzzle = '*ion fri*'
-    puzzle = 'pa*ma vi*nt'
+    # puzzle = '*stro sou*er'
+    # puzzle = '*ion fri*'
+    # puzzle = 'pa*ma vi*nt'
 
     puzzler(puzzle)
